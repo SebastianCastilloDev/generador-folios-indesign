02f369defffa1c05e9e7281671deeaea90df14f8
Sebastian Castillo <tantrum5535@gmail.com>
2023-08-16 17:43:16 -0400

    reorganizacion del código y correccion gitignore

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
+#!/usr/bin/env node
+
 import fs from 'fs';
+import inquirer from 'inquirer'
 
-const folioInicial = 181
-const folioFinal = 200
-const moldes = 4
-const digitos = 6
-const caracterRelleno = '0'
-const sobrantes = 3
-const rango = folioFinal - folioInicial + 1
-const filas = rango / moldes
+    const answers = inquirer.prompt([
+        {
+            type: 'number',
+            message: 'Folio inicial',
+            name:'folioInicial'
+        },
+        {
+            type: 'number',
+            message: 'Folio Final',
+            name:'folioFinal'
+        },
+        {
+            type: 'number',
+            message: 'Moldes',
+            name:'moldes'
+        },
+        {
+            type: 'number',
+            message: 'Digitos',
+            name:'digitos',
+            default: 6
+        },
+        {
+            type: 'input',
+            message: 'Caracter de relleno',
+            name:'caracterRelleno',
+            default: '0'
+        },
+        {
+            type: 'number',
+            message: 'Sobrantes',
+            name:'sobrantes',
+            default: 3
+        }
+    ]).then((answers) => {
+        generarFolios(answers)
+    })
+    
+function generarFolios(answers) {
+    const {folioInicial, folioFinal, moldes, digitos,caracterRelleno, sobrantes} = answers
 
-const file = `./Ffolio ${ folioInicial } al ${ folioFinal } ${moldes} moldes.csv`
-
-function validate(){
-    if (folioFinal < folioInicial){
-        console.log('Folio inicial debe ser menor que el folio final')
-        return false
-    } else if (!Number.isInteger(filas)){
-        console.log('rango de folios no cuadra con el numero de moldes')
-    } else {
-        return true
-    }
-}
-
-function generarFolios() {
-
-    if (validate()) {
+    const rango = folioFinal - folioInicial + 1
+    const filas = rango / moldes
+    const folderName = '/Users/sebastiancastillo/Library/CloudStorage/OneDrive-Personal/trabajos-talonarios'
+    const file = `${folderName}/Ffolio ${ folioInicial } al ${ folioFinal } ${moldes} moldes ${Date.now()}.csv`
+    
+    if (validate(answers)) {
 
         //escribe la primer linea
-        fs.writeFileSync(file, linea1())
+        fs.writeFileSync(file, linea1(moldes))
         
         let i = 0 // recorre filas de folios
         while (i < filas) {
-            fs.appendFileSync(file,linea(folioInicial+i, moldes))
+            fs.appendFileSync(file,linea(folioInicial+i, moldes, filas, digitos, caracterRelleno))
             i++
         }
         
         i = 0 // recorre filas de sobrantes
         while (i < sobrantes) {
-            fs.appendFileSync(file,lineaSobrante())
+            fs.appendFileSync(file,lineaSobrante(moldes))
             i++
         }
+        console.log(`${file} creado exitosamente`)
     }
-    
 }
 
-generarFolios()
-
-
-
 //crear linea
-function linea(folio, moldes) {
-
+function linea(folio, moldes, filas, digitos, caracterRelleno) {
     let linea=''
     let i = 1
     
     while (i<=moldes){
         if (i<moldes){
-            linea += `${formatearFolio(folio+filas*(i-1))},`
+            linea += `${formatearFolio(folio+filas*(i-1), digitos, caracterRelleno)},`
         } else if (i == moldes){
-            linea += `${formatearFolio(folio+filas*(i-1))}\n` //no agrega la ultima "coma" al final
+            linea += `${formatearFolio(folio+filas*(i-1), digitos, caracterRelleno)}\n` //no agrega la ultima "coma" al final
         }
         i++
     }
@@ -66,11 +86,11 @@ function linea(folio, moldes) {
 }
 
 // fs.appendFileSync(file,formatearFolio(folio+i))
-function formatearFolio(folio) {
+function formatearFolio(folio, digitos, caracterRelleno) {
     return folio.toString().padStart(digitos,caracterRelleno)
 }
 
-function linea1(){
+function linea1(moldes){
     let linea=''
     let i = 1
     while (i<=moldes){
@@ -85,7 +105,7 @@ function linea1(){
     return linea
 }
 
-function lineaSobrante(){
+function lineaSobrante(moldes){
     let linea=''
     let i = 1
     while (i<=moldes){
@@ -98,4 +118,20 @@ function lineaSobrante(){
         // console.log(linea)
     }
     return linea
+}
+
+function validate(answers){
+    const {folioInicial, folioFinal, moldes} = answers
+
+    const rango = folioFinal - folioInicial + 1
+    const filas = rango / moldes
+
+    if (folioFinal < folioInicial){
+        console.log('Folio inicial debe ser menor que el folio final')
+        return false
+    } else if (!Number.isInteger(filas)){
+        console.log('rango de folios no cuadra con el numero de moldes')
+    } else {
+        return true
+    }
 }
